eb3b6b88ec82d5ea067cab12804ebc9c21166228
Caroline Longnecker <cvrieous@gmail.com>
2020-09-06 23:10:20 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates_length_of :phone_number, is: 10
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates_length_of :content, minimum: 250
+  validates_length_of :summary, maximum: 250
+  validates_inclusion_of :category, in: %w[Fiction Non-Fiction]
+  validate :post_is_clickbait
+
+  def post_is_clickbait
+    return unless title
+
+    unless ["Won't Believe", 'Secret', 'Top', 'Guess'].any? { |s| title.include?(s) }
+      errors.add(:title, 'isn\'t clickbait')
+    end
+  end
 end
